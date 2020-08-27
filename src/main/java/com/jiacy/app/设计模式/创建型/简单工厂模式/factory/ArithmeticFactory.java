709107d5cad4580b78705e119f53caa8449348ee
jiacywork <jiacywork@outlook.com>
2020-08-27 13:07:48 +0800

    设计模式代码修改和相应README文件内容更新

--- src/main/java/com/jiacy/app/设计模式/创建型/简单工厂模式/factory/ArithmeticFactory.java
+++ src/main/java/com/jiacy/app/设计模式/创建型/简单工厂模式/factory/ArithmeticFactory.java
@@ -6,6 +6,7 @@ import com.jiacy.app.设计模式.创建型.简单工厂模式.arithmetic.Arithm
 
 public class ArithmeticFactory {
 
+    // 常规简单工厂模式
     public Arithmetic getArithmetic(String opr) {
         Arithmetic arithmetic = null;
         if ("+".equals(opr))
@@ -14,6 +15,21 @@ public class ArithmeticFactory {
             arithmetic = new ArithmeticSubtraction();
         else
             throw new NullPointerException("暂不支持此算法");
+        return arithmetic;
+    }
+
+    // 优化方案：使用反射，关闭工厂类修改，使满足开闭原则
+    public Arithmetic getArithmetic(Class clazz) {
+        Arithmetic arithmetic = null;
+        try {
+            arithmetic = (Arithmetic) Class.forName(clazz.getName()).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return arithmetic;
     }
 
